db1e874fb72d6f6c0c125c612d21bba5c07cd0ab
OsavivV <oklan002blog@gmail.com>
2023-05-03 13:33:58 -0500

    fin carruseles de imagenes

--- src/public/js/carrousel.js
+++ src/public/js/carrousel.js
@@ -26,3 +26,65 @@ function updateCarousel() {
 }
 
 updateCarousel();
+
+// -----------------Segundo Carrousel Llamado  CarouselTwo
+
+const imagesTwo = document.querySelectorAll(".trendingTwo");
+const prevBtnTwo = document.getElementById("prev2");
+const nextBtnTwo = document.getElementById("next2");
+let currentIndexTwo = 0;
+
+prevBtnTwo.addEventListener("click", (e) => {
+	e.preventDefault();
+	currentIndexTwo = (currentIndexTwo - 1 + imagesTwo.length) % imagesTwo.length;
+	updateCarouselTwo();
+});
+
+nextBtnTwo.addEventListener("click", (e) => {
+	e.preventDefault();
+	currentIndexTwo = (currentIndexTwo + 1) % imagesTwo.length;
+	updateCarouselTwo();
+});
+
+function updateCarouselTwo() {
+	for (let i = 0; i < imagesTwo.length; i++) {
+		if (i >= currentIndexTwo && i < currentIndexTwo + 3) {
+			imagesTwo[i].style.display = "block";
+		} else {
+			imagesTwo[i].style.display = "none";
+		 }
+	}
+}
+
+updateCarouselTwo();
+
+//------------------------ Tercer Carrousel Llamado carouselThree
+
+const imagesThree = document.querySelectorAll(".trendingThree");
+const prevBtnThree = document.getElementById("prev3");
+const nextBtnThree = document.getElementById("next3");
+let currentIndexThree = 0;
+
+prevBtnThree.addEventListener("click", (e) => {
+	e.preventDefault();
+	currentIndexThree = (currentIndexThree - 1 + imagesThree.length) % imagesThree.length;
+	updateCarouselThree();
+});
+
+nextBtnThree.addEventListener("click", (e) => {
+	e.preventDefault();
+	currentIndexThree = (currentIndexThree + 1) % imagesThree.length;
+	updateCarouselThree();
+});
+
+function updateCarouselThree() {
+	for (let i = 0; i < imagesThree.length; i++) {
+		if (i >= currentIndexThree && i < currentIndexThree + 3) {
+			imagesThree[i].style.display = "block";
+		} else {
+			imagesThree[i].style.display = "none";
+		 }
+	}
+}
+
+updateCarouselThree();
