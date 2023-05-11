dd58c36cb5375358863c0cb9debd4706a8c07d42
OsavivV <oklan002blog@gmail.com>
2023-05-11 16:40:14 -0500

    validations & express validator

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,27 +2,34 @@ const path = require('path');
 const fs = require('fs');
 // // const bcrypt = require('bcryptjs');
 // const { json } = require('express');
-// const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator') 
 
+const User = require ('../database/models/User')
 
 const controller = {
     register: (req, res) => {
         res.render('./users/loginRegister');
     },
-    // registered: (req, res) => {
-    // res.render('/');
-    // },
+
+    registered: (req, res) => {
+        const resultValidation = validationResult (req);
+
+        if (resultValidation.errors.length > 0 ) {
+            return res.render ( './users/loginRegister' , {
+                error: resultValidation.mapped(),
+                oldData: req.body
+            });
+        }
+    
+    },
 
     login: (req, res) => {
         res.render('./users/loginRegister');
     },
 
-    // logged: (req, res) => {
-    //         res.render('/');
-    // },
-    // profile: (req, res) => {
-    //     res.render('/');
-    // },
+    profile: (req, res) => {
+        res.render('/');
+    },
 }
 
 module.exports = controller ;
---
+++ src/middleware/validMiddleware.js
@@ -0,0 +1,15 @@
+const { body } = require('express-validator')
+
+
+module.exports = [
+    body('firstName').notEmpty().withMessage('*Tienes que escribir un nombre*'),
+    body('lastName').notEmpty().withMessage('*Tienes que escribir un apellido*'),
+    body('email').notEmpty().withMessage('*Tienes que escribir un correo electrónico*').bail().isEmail().withMessage('*Tienes que escribir un formato de correo válido*'),
+    body('password').notEmpty().withMessage('*Tienes que escribir una contraseña*').bail().isLength({min:8}).withMessage('*La contraseña debe tener minimo 8 caracteres*'),
+]
+
+// const validateLogin = [
+//     body('email').notEmpty().withMessage('*Tienes que escribir un correo electrónico*').bail().isEmail().withMessage('*Tienes que escribir un formato de correo válido*'),
+//     body('password').notEmpty().withMessage('*Tienes que escribir una contraseña*').bail().isLength({min:8}).withMessage('*La contraseña debe tener mínimo 8 caracteres*')   // Minimo 8 caracteres
+// ];
+
